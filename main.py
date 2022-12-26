93a683d368927b8756f383e8d31a42e4ccee9043
Narongkorn Kitrungrot <psuwit.narongkron.nk@gmail.com>
2022-12-26 22:17:29 +0700

    Change DB Style

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
 from src.crud import PaymentMethod
+from typing import Union
 
 app = FastAPI()
+
+# Header and Encrupt
 
 @app.get("/")
 async def root():
@@ -10,15 +12,37 @@ async def root():
 
 @app.get("/check")
 async def checks():
-    return {"value": PaymentMethod.checkAll()}
+    return {"value": PaymentMethod().checkAll()}
 
 @app.get("/check/{date}")
-async def check(date : int):
+async def check(date : str):
     return {"date": date,
-            "value": PaymentMethod.checkSingle(date=date)}
+            "value": PaymentMethod().checkSingle(date=date)}
 
-@app.post("/upload/{date}/{types}/{money}")
-async def upload(date: int, types: str, money: float):
+@app.post("/expense/{date}/{types}/{money}")
+async def expense(date: str, types: str, money: float, description: Union[str, None] = None):
     return {"date": date,
-            "value": PaymentMethod.upload(date=date, types=types, money=money)}
+            "value": PaymentMethod().expense(date=date, time='00:00', amount=money, types=types, description=description)}
 
+@app.post("/income/{date}/{types}/{money}")
+async def income(date: str, types: str, money: float, description: Union[str, None] = None):
+    return {"date": date,
+            "value": PaymentMethod().expense(date=date, time='00:00', amount=money, types=types, description=description)}
+
+@app.post("/transactions/{date}/{types}/{money}")
+async def transactions(date: str, types: str, money: float, description: Union[str, None] = None):
+    return {"date": date,
+            "value": PaymentMethod().expense(date=date, time='00:00', amount=money, types=types, description=description)}
+
+
+'''
+checks
+sum of amount 
+balance this month
+
+check
+select transaction in date 
+
+expense
+
+'''
