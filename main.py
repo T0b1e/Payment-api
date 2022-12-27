f2f54681b577050308686ecef4871637aa31631c
Narongkorn Kitrungrot <psuwit.narongkron.nk@gmail.com>
2022-12-27 21:37:59 +0700

    Database Working fine with Casual Case

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from src.crud import PaymentMethod
 from typing import Union
+
+# Third party
+from datetime import datetime
 
 app = FastAPI()
 
 # Header and Encrupt
+timeNow = datetime.now()
+presentDate = timeNow.strftime("%Y-%m-%d")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "It's Working fine"}
 
 @app.get("/check")
 async def checks():
@@ -21,28 +26,23 @@ async def check(date : str):
 
 @app.post("/expense/{types}/{money}")
 async def expense(types: str, money: float, description: Union[str, None] = None):
-    return {"date": "2022-12-27", #TODO
-            "value": PaymentMethod().expense(amount=money, types=types, description=description)}
+    value = PaymentMethod().expense(amount=money, types=types, description=description)
+    if not value:
+        raise HTTPException(status_code=404, detail="Error :(")
+
+    return {"date": presentDate, 
+            "value": value}
 
 @app.post("/income/{types}/{money}")
 async def income(types: str, money: float, description: Union[str, None] = None):
-    return {"date": "2022-12-27", #TODO
-            "value": PaymentMethod().income(amount=money, types=types, description=description)}
+    value = PaymentMethod().income(amount=money, types=types, description=description)
+    if not value:
+        raise HTTPException(status_code=404, detail="Error :(")
+
+    return {"date": presentDate,
+            "value": value}
 
 @app.post("/transactions/{types}/{money}")
 async def transactions(types: str, money: float, description: Union[str, None] = None):
-    return {"date": "2022-12-27", #TODO
+    return {"date": presentDate,
             "value": PaymentMethod().transactions(amount=money, types=types, description=description)}
-
-
-'''
-checks
-sum of amount 
-balance this month
-
-check
-select transaction in date 
-
-expense
-
-'''
